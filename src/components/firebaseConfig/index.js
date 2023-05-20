b375c59a6faf264a11e0b572d471461d4258b5f5
Azrael <124645344+Azrael24@users.noreply.github.com>
2023-05-19 21:47:05 -0700

    Update index.js

--- src/components/firebaseConfig/index.js
+++ src/components/firebaseConfig/index.js
@@ -3,15 +3,16 @@ import { getDatabase } from "firebase/database";
 
 // Your web app's Firebase configuration
 function StartFirebase(){
-const firebaseConfig = {
-  apiKey: "AIzaSyA8MUfwbFU2MpcYgUqbajOIgaPH_JLmc2c",
-  authDomain: "crud-firebase-66d0b.firebaseapp.com",
-  databaseURL: "https://crud-firebase-66d0b-default-rtdb.firebaseio.com",
-  projectId: "crud-firebase-66d0b",
-  storageBucket: "crud-firebase-66d0b.appspot.com",
-  messagingSenderId: "56961373178",
-  appId: "1:56961373178:web:4c04c2eaf020293a660a43"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyA8MUfwbFU2MpcYgUqbajOIgaPH_JLmc2c",
+    authDomain: "crud-firebase-66d0b.firebaseapp.com",
+    databaseURL: "https://crud-firebase-66d0b-default-rtdb.firebaseio.com",
+    projectId: "crud-firebase-66d0b",
+    storageBucket: "crud-firebase-66d0b.appspot.com",
+    messagingSenderId: "56961373178",
+    appId: "1:56961373178:web:4c04c2eaf020293a660a43"
+  };
+  
   
   // Initialize Firebase
   const app = initializeApp(firebaseConfig);
